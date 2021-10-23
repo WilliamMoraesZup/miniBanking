201e3c67df22d5331fb72743d438e86656e2c644
Bruno Vieira <brunoscvieira@gmail.com>
2021-10-23 17:25:45 -0300

    creating main project

---
+++ miniBankingTests/miniBankingTests.swift
@@ -0,0 +1,33 @@
+//
+//  miniBankingTests.swift
+//  miniBankingTests
+//
+//  Created by Bruno Vieira on 23/10/21.
+//
+
+import XCTest
+@testable import miniBanking
+
+class miniBankingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
